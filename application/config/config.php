fe5832507ff716034511ce369426c99c11c0a841
wings <1073730081@qq.com>
2018-05-19 14:42:45 +0800

    from music_web

---
+++ application/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$GLOBALS['config']['password'] = "";
+$GLOBALS['config']['host'] = "localhost";
+$GLOBALS['config']['user'] = 'root';
+$GLOBALS['config']['dbname'] = '';
+$GLOBALS['config']['port'] = '3306';
+$GLOBALS['config']['charset'] = 'UTF8';
+$GLOBALS['config']['prefix'] = '';
+?>
---
+++ index.html
@@ -0,0 +1,7 @@
+<?php
+
+require "framework/core/Framework.class.php";
+
+Framework::run();
+
+?>
